029f83a99dbfcb52414e1209277dde56dc3b608f
Ian White <ian.w.white@gmail.com>
2009-05-30 13:23:23 +0100

    Added order! method that takjes records or ids, and splats

--- lib/ardes/acts_as_list_order_by_ids.rb
+++ lib/ardes/acts_as_list_order_by_ids.rb
@@ -1,15 +1,23 @@
 module Ardes #:nodoc:
-  # when acts_as_list specified, a class method order_by_ids is appended to the
-  # class
+  # when acts_as_list specified, class methods
+  #
+  #  * order_by_ids
+  #  * order!
+  #
+  # are appended to the class (and thus any ordered associations as well)
   module ActsAsListOrderByIds
     def self.included(base)
       base.class_eval do
         class<<self
           def acts_as_list_with_order_by_ids(*args, &block)
-            returning acts_as_list_without_order_by_ids(*args, &block) do 
+            returning acts_as_list_without_order_by_ids(*args, &block) do
+              
               options = args.extract_options!
               column = options[:column] || 'position'
+
               module_eval <<-end_eval, __FILE__, __LINE__
+                
+                # Order the collection by ids
                 def self.order_by_ids(ids)
                   transaction do
                     ids.each_index do |i|
@@ -19,9 +27,16 @@ module Ardes #:nodoc:
                     end
                   end
                 end
+
+                # Order the collection by records or ids
+                def self.order!(*records)
+                  order_by_ids(records.flatten.map(&:id))
+                end
+                
               end_eval
             end
           end
+
           alias_method_chain :acts_as_list, :order_by_ids
         end
       end
