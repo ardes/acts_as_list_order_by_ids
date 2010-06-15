b284c8583b6d01f7756147bda93bdfb7a8a0afe4
Ian White <ian.w.white@gmail.com>
2010-06-15 11:46:20 +0100

    Make init be aware of gem installed versions, and not bork if acts_as_list is not found to resolve gem dependency issues

--- init.rb
+++ init.rb
@@ -1,3 +1,11 @@
-require 'ardes/acts_as_list_order_by_ids'
+begin
+  require 'acts_as_list'
+rescue LoadError
+end
 
-ActiveRecord::Base.send :include, Ardes::ActsAsListOrderByIds
+if ActiveRecord::Base.respond_to?(:acts_as_list)
+  require 'ardes/acts_as_list_order_by_ids'
+  ActiveRecord::Base.send :include, Ardes::ActsAsListOrderByIds
+else
+  puts "Install acts_as_list via gem or plugin to use acts_as_list_order_by_ids"
+end
